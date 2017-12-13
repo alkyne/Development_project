89f3a1a92c60551196fdeedf54f055ec76ea7e6c
alkyne <alkyne@users.noreply.github.com>
2017-12-14 02:04:32 +0900

    Update list.java

--- Project/src/Food/list.java
+++ Project/src/Food/list.java
@@ -3,6 +3,9 @@ package Food;
 import java.awt.Component;
 import javax.swing.*;
 import javax.swing.table.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class list extends JDialog{ 
@@ -18,12 +21,14 @@ public class list extends JDialog{
   private DefaultTableModel dtm;
   private JTable jtable;
   private JScrollPane jsp;     
+  private JButton ok_button;
 
   public list() {
 
       jf = new JFrame("테이블에 버튼 추가 이벤트");
       jf.setLocationRelativeTo(null);
-      jf.setSize(500, 300);
+      jf.setBounds(100,500,500, 300);
+      
       
       //dtm = new DefaultTableModel(new Object[][] {{ "데이터1", database_test.restname.get(0)},
       //                                            { "데이터2", database_test.restname.get(1)}, }, new Object[]{ "필드1", "필드2"});
@@ -39,11 +44,21 @@ public class list extends JDialog{
 
       jtable.getColumnModel().getColumn(0).setCellRenderer(new TableCell());
       jtable.getColumnModel().getColumn(0).setCellEditor(new TableCell());
-      jf.add(jsp);        
+      jf.getContentPane().add(jsp, BorderLayout.CENTER);        
+      
+      ok_button = new JButton("확 인");
+      ok_button.addActionListener(new ActionListener() {
+      	public void actionPerformed(ActionEvent e) {
+      		race_game race_game = new race_game();
+      		database.dbconn();
+      		jf.setVisible(false);
+      	}
+      });
+      jf.getContentPane().add(ok_button, BorderLayout.SOUTH);
 
       jf.setVisible(true);
       
-      new race_game();
+     // new race_game();
       
 
   }   
@@ -97,3 +112,4 @@ public class list extends JDialog{
   } 
 
 }
+
