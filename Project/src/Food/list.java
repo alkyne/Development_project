edc53e39a5aad78b795d8526372c6870c441ca41
alkyne <alkyne@users.noreply.github.com>
2017-12-14 22:18:16 +0900

    Update list.java

--- Project/src/Food/list.java
+++ Project/src/Food/list.java
@@ -1,113 +1,114 @@
 package Food;
 
-import java.awt.*;
-
+import java.awt.Component;
 import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-import java.awt.event.*;
 
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
+public class list extends JDialog{ 
 
-public class money extends JFrame {
+   public static void main(String[] args) {
 
-	
-	
-	private DefaultTableModel dtm;
-    private JTable jtable;
-    private JScrollPane jsp;	
-	
-	/**
-	 * Create the frame.
-	 */
-	public money() {
-		super("가격 결정");
-		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		Container c = this.getContentPane();
-		c.setLayout(null); //배치관리자 없음
-		
-		JLabel m0 = new JLabel("가격 결정");  
-		m0.setHorizontalAlignment(SwingConstants.CENTER);
-		m0.setLocation(0, 0);
-		m0.setSize(300, 200);
-		m0.setBackground(Color.white);
-		m0.setFont(new Font("휴먼엑스포", Font.BOLD, 30));
-		m0.setBounds(246, 10, 300, 200);
-		
-		JButton m1 = new JButton("저가");   //메뉴판 선택
-		m1.setLocation(60, 183);
-		m1.setSize(198, 111);
-		m1.setBackground(Color.white);
-		m1.setFont(new Font("휴먼엑스포", Font.BOLD, 30));
-		m1.addActionListener(new ActionListener() {  // 가격 버튼을 눌렀을때
-			public void actionPerformed(ActionEvent e) {
-				main.money_num = 0;
-				database.dbconn();
-				list list = new Food.list();
-			}
-		});		
-		
-		JButton m2 = new JButton("중간");
-		m2.setFont(new Font("휴먼엑스포", Font.BOLD, 30));
-		m2.setBackground(Color.WHITE);
-		m2.setBounds(300, 183, 198, 111);
-		m2.addActionListener(new ActionListener() {  // 가격 버튼을 눌렀을때
-			public void actionPerformed(ActionEvent e) {
-				main.money_num = 1;
-				database.dbconn();
-				list list = new Food.list();
-			}
-		});
-		
-		JButton m3 = new JButton("고가");
-		m3.setFont(new Font("휴먼엑스포", Font.BOLD, 30));
-		m3.setBackground(Color.WHITE);
-		m3.setBounds(538, 183, 198, 111);
-		m3.addActionListener(new ActionListener() {  // 가격 버튼을 눌렀을때
-			public void actionPerformed(ActionEvent e) {
-				main.money_num = 2;
-				database.dbconn();
-				list list = new Food.list();
-			}
-		});
+      database.dbconn();
+      new list();
+      
+   } 
 
-		// main button
-		JButton m10 = new JButton("Main");
-		m10.setFont(new Font("휴먼엑스포", Font.BOLD, 20));
-		m10.setBackground(Color.WHITE);
-		m10.setBounds(654, 725, 118, 51);
-		m10.addActionListener(new ActionListener() {  //Main 버튼을 눌렀을때
-			public void actionPerformed(ActionEvent e) {
-				main main= new Food.main();  // money 클래스 실행
-				setVisible(false);				
-			}
-		});
-		
-		getContentPane().add(m10);
-		getContentPane().add(m0);
-		getContentPane().add(m1);
-		getContentPane().add(m2);
-		getContentPane().add(m3);	
-		
-		
-		setSize(800, 840);
-		setVisible(true);
-		
-	
-	}
+  private JFrame jf;
+  private DefaultTableModel dtm;
+  private JTable jtable;
+  private JScrollPane jsp;     
+  private JButton ok_button;
 
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		
-		new money();
-		
-		
-	}
+  public list() {
+
+      jf = new JFrame("테이블에 버튼 추가 이벤트");
+      jf.setLocationRelativeTo(null);
+      jf.setBounds(100,500,500, 300);
+      
+      
+      //dtm = new DefaultTableModel(new Object[][] {{ "데이터1", database_test.restname.get(0)},
+      //                                            { "데이터2", database_test.restname.get(1)}, }, new Object[]{ "필드1", "필드2"});
+      
+      String colNames[] = {"필드1","필드2"}; 
+      dtm = new DefaultTableModel(colNames, 0);
+      
+      for(int i=0; i<database.noOfrest; i++)
+         dtm.addRow(new Object[] {"데이터", database.restname.get(i)});
+
+      jtable = new JTable(dtm);
+      jsp = new JScrollPane(jtable); 
+
+      jtable.getColumnModel().getColumn(0).setCellRenderer(new TableCell());
+      jtable.getColumnModel().getColumn(0).setCellEditor(new TableCell());
+      jf.getContentPane().add(jsp, BorderLayout.CENTER);        
+      
+      ok_button = new JButton("확 인");
+      ok_button.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            race_game race_game = new race_game();
+            database.dbconn();
+            jf.setVisible(false);
+         }
+      });
+      jf.getContentPane().add(ok_button, BorderLayout.SOUTH);
+
+      jf.setVisible(true);
+      
+     // new race_game();
+      
+
+  }   
+
+  
+
+  class TableCell extends AbstractCellEditor implements TableCellEditor, TableCellRenderer{
+
+      JCheckBox jc;
+     
+
+      public TableCell() {
+
+          // TODO Auto-generated constructor stub
+
+          jc = new JCheckBox("버튼");
+          jc.addActionListener(e -> {
+              System.out.println(jtable.getValueAt(jtable.getSelectedRow(), 1));
+          });
+      }
+
+       
+      @Override
+
+      public Object getCellEditorValue() {
+          // TODO Auto-generated method stub
+
+          return null;
+
+      }
+
+      @Override
+
+      public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+          // TODO Auto-generated method stub
+
+          return jc;
+      }
+
+      @Override
+
+      public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+
+          // TODO Auto-generated method stub
+
+          return jc;
+
+      }         
+
+  } 
+
 }
-
